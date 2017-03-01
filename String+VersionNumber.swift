a8ad0c20784d3daf47104ebed8f9cc0186d6ee54
watr <watr@users.noreply.github.com>
2017-03-01 10:20:54 +0900

    edit comment.

--- String+VersionNumber.swift
+++ String+VersionNumber.swift
@@ -1,6 +1,5 @@
 //
-//  String-VersionNumberComparison.swift
-//  String-VersionNumberComparison
+//  String+VersionNumber.swift
 //
 //  Created by hashimoto wataru on 2017/02/28.
 //  Copyright © 2017年 wataruhash.info. All rights reserved.
